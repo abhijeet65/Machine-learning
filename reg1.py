97809788acbb40abadf7fdf872b377904658c4e3
starboy21 <abhijeet.intern@gmail.com>
2019-02-22 00:42:53 +0530

    linear regression done

--- reg1.py
+++ reg1.py
@@ -11,7 +11,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dataset=pd.read_csv('Salary_Data.csv')
-X=dataset.iloc[:].values
+X=dataset.iloc[:,:-1].values
 y=dataset.iloc[:, 1].values
 
-print(X)
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
+ 
+#fitting linear regression model in traning set
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+#predicting test set results
+
+y_pred = regressor.predict(X_test)
+
+
+
